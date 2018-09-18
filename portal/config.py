7ec72037d8e3e6a5aafc950339358ce61e9166ca
Julian Schauder <julian.schauder@credativ.de>
2018-09-18 07:38:07 +0200

    Added Comments to readme

--- portal/config.py
+++ portal/config.py
@@ -1,13 +1,23 @@
-debug=True
-usemin=False
-appkey="TEST"
-application_root = "/portal"
+#######
+debug=True                      # Enable this to flip flasks internal debug-mode.
+                                # Greatly slows down the system but allows for on-line
+                                # Reload of templates.
+
+usemin=False                    # Use Minified versions of JS-Libraries where available.
+
+appkey="TEST"                   # App-Key for encryption. Change in production.
+
+application_root = "/portal"    # Root-Folder this application resides in.
+                                # Should match with apache.conf.
+
+# Hosts we consider beeing part of a Cluster.
 hosts=[
         {"address":"localhost:4433"},
         {"address":"localhorst"},
         {"address":"127.0.0.1:4433"},
         ]
 
+# Lefthand-Side Navigation to the most important links.
 navigation=[
         {"url" :"/", "label" :"Home"},
         {"url" :"/", "label" :"Support"},
@@ -15,6 +25,7 @@ navigation=[
         {"url" :"/", "label" :"postgresql.org"}, 
         ]
 
+# Detail-views deeplinks to elephant-shed components for a given cluster.
 deeplinks=[
         {"url" :"/", "label" : "Monitoring", "image": "button_grafana.png"},
         {"url" :"/", "label" : "Administration", "image": "button_pgadmin.png"},
