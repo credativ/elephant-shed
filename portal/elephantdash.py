93bc8ad704053617116fea25cb4c658cadaa4a42
Julian Schauder <julian.schauder@credativ.de>
2018-09-19 09:57:14 +0200

    * Added early js-refactoring. * Added pg_ctl-function * Added early Create-Functionality * Added the Management-Host as Requestproxy for Security and Usability Reasons * Added pass-auth to apache-conf to allow basic-auth forwarding for rest-calls.

--- portal/elephantdash.py
+++ portal/elephantdash.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, Response
 #from flask_login import LoginManager, login_required, login_user, current_user, logout_user
 #from flask_cors import CORS
+
 import os
 import psycopg2
 import psycopg2.extras
 import config
 import json
 import time
+import urllib.request
+import base64
+import ssl
 #import jinja2
 #from .views.dash import view
 
@@ -35,6 +39,55 @@ def index():
     response = Response ( render_template('clusters.html', environment=env) )
     #response.headers['Access-Control-Allow-Origin'] = 'http://127.0.0.1:15432/cluster/'
     return response
+
+
+def buildRequest(url, request):
+    method = request.method
+    payload = "{}"
+    try:
+        payload = json.dumps( request.json )
+    except:
+        pass
+    payload = payload.encode('utf-8')
+    req = urllib.request.Request(url)
+    req.add_header('Content-Type', 'application/json; charset=utf-8')
+    req.add_header('Content-Length', len(payload))
+    req.add_header('Authorization', request.environ.get('HTTP_AUTHORIZATION') )
+    req.method = method
+
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE    # FIX ME  ## TODO
+    print (payload)
+    try:
+        ret =  urllib.request.urlopen( req, payload, context=ctx )
+    except Exception as e:
+        #print( "-----"  )
+        #print( e.code   )
+        #print( e.read() )
+        try:
+            return Response(e.read(), mimetype='application/json', status=e.code)
+        except Exception as e2:
+            return Response(json.dumps({ "stderr" : "UNRECOVERABLE ERROR", "msg" : str(e) }) , mimetype='application/json', status=500)
+    return Response(ret.read(), mimetype='application/json')
+
+
+@app.route("/pgapi/proxy/<host>", methods=['POST','GET','PATCH', 'PUT','DELETE'] )
+def pgapi_proxy(host):
+    print(host)
+    return  buildRequest(
+                    'https://'+host+'/pgapi/cluster', 
+                    request
+                    )
+
+@app.route("/pgapi/proxy/<host>/<version>/<cluster>", methods=['POST','GET','PATCH','PUT','DELETE'] )
+def pgapi_proxy_cluster(host,version,cluster):
+    print (host, version, cluster)
+    return buildRequest(
+                    'https://'+host+'/pgapi/cluster/'+version+'/'+cluster, 
+                    request
+                    )
+
 
 @app.route("/detail/<host>")
 def details_host(host):
