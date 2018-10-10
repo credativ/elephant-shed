06b7e203b3ee0fab221c32ba87b19479517ab94c
Julian Schauder <julian.schauder@credativ.de>
2018-10-10 15:09:51 +0200

    Pre-presentation cleanup and new mockups to backup&pg_hba

--- portal/config.py
+++ portal/config.py
@@ -16,21 +16,26 @@ hosts=[
         ]
 
 detailview_systeminformation=[
-        {"label" : "HostFQDN",      "data" : "pg_hostname pg_fqdn"},
-        {"label" : "CpuCount",  "formoverride" : '<span class="pg_cpu_config pg_cpu_count pg_addgraph"></span>'},
-        {"label" : "OS",        "data" : "pg_uname pg_0"},
-        {"label" : "Memory",       "formoverride": '<span class="pg_virtual_memory pg_total pg_addgraph"></span> (<span class="pg_virtual_memory pg_percent"></span>% Used)'}, 
+        {"label" : "Hostname (FQDN)",      "data" : "pg_hostname pg_fqdn"},
+        {"label" : "CPU Count",  "data" : "pg_cpu_config pg_cpu_count"},
+        {"label" : "Kernel",        "formoverride" : '<span class="pg_uname pg_kernel_name"></span> <span class="pg_uname pg_kernel_version"</span> <span class="pg_uname pg_machine"></span>'},
+        {"label" : "Load Average",  "formoverride" : '<span class="pg_load pg_15min pg_addgraph"></span>'},
+        {"label" : "Memory",       "formoverride": '<span class="pg_virtual_memory pg_total pg_addgraph"></span> (<span class="pg_virtual_memory pg_percent"></span>% Used)'},
+        {"label" : "Disk Usage (%)", "data" : "pg_disk_usage pg__s_ pg_percent"},
+
+
         ]
 
 detailview_clusterinformation=[
+        #{"label" : "data_directory",  "data" : "pg_config pg_data_directory"},
+        {"label" : "listen_addresses",  "data" : "pg_config pg_listen_addresses" },
+        {"label" : "port",  "data" : "pg_config pg_port" },
         {"label" : "shared_buffers",  "data" : "pg_config pg_shared_buffers" },
         {"label" : "max_connections",  "data" : "pg_config pg_max_connections"},
+        {"label" : "work_mem",  "data" : "pg_config pg_work_mem"},
         {"label" : "wal_level",  "data" : "pg_config pg_wal_level"},
         {"label" : "max_wal_size",  "data" : "pg_config pg_max_wal_size"},
         {"label" : "synchronous_commit",  "data" : "pg_config pg_synchronous_commit"},
-        {"label" : "data_directory",  "data" : "pg_config pg_data_dir"},
-        {"label" : "JIT",  "data" : "pg_config pg_jit"},
-        {"label" : "max_parallel_workers_per_gather",  "data" : "pg_config pg_max_parallel_workers_per_gather"},
         ]
 
 
@@ -46,14 +51,14 @@ navigation=[
 
 # Detail-views deeplinks to elephant-shed components for a given cluster.
 deeplinks=[
-        {"url" :"/grafana/", "label" : "Monitoring", "image": "button_grafana.png"},
-        {"url" :"/pgadmin4/browser", "label" : "Administration", "image": "button_pgadmin.png"},
-        {"url" :"/prometheus/", "label" : "Reports", "image": "button_pgbadger.png"},
-        {"url" :"/pgbadger/", "label" : "Metrics", "image": "button_prometheus.png"},
-        {"url" :"/", "label" : "Backups TODO", "image": "button_pgbackrest.png"},
-        {"url" :"/shellinabox/", "label" : "Shell", "image": "button_shellinabox.png"},
-        {"url" :"/system", "label" : "Cockpit", "image": "button_cockpit.png"},
-        {"url" :"/doc/html/", "label" : "Documentation", "image": "button_doc.png"},
+        {"url" :":4433/grafana/", "label" : "Monitoring", "image": "button_grafana.png"},
+        {"url" :":4433/pgadmin4/browser", "label" : "Administration", "image": "button_pgadmin.png"},
+        {"url" :":4433/pgbadger/", "label" : "Reports", "image": "button_pgbadger.png"},
+        # {"url" :":4433/prometheus/", "label" : "Metrics", "image": "button_prometheus.png"},
+        # {"url" :":4433/", "label" : "Backups TODO", "image": "button_pgbackrest.png"},
+        {"url" :":4433/shellinabox/", "label" : "Shell", "image": "button_shellinabox.png"},
+        {"url" :":4433/system", "label" : "Cockpit", "image": "button_cockpit.png"},
+        {"url" :":4433/doc/html/", "label" : "Documentation", "image": "button_doc.png"},
 
 
         ]
