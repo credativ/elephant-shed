79932cee0917217f0b9196fe388319b2e9c0c7f7
Christoph Berg <christoph.berg@credativ.de>
2018-12-04 15:34:29 +0100

    Refactor build handling

--- aptly/include_packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-REPO="$1"
-SERVER="http://172.26.251.70:8080"
-
-set -x
-curl -fX POST "$SERVER/api/repos/$REPO/file/$REPO?forceReplace=1"
--- aptly/publish_packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-DISTRIBUTION="$1"
-SERVER="http://172.26.251.70:8080"
-
-set -x
-curl -fX PUT -H 'Content-Type: application/json' --data '{"ForceOverwrite":true}' $SERVER/api/publish/:./$DISTRIBUTION
