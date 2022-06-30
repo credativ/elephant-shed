89181008214ebfc8c518880f8a2b0ddae04844c7
Christoph Berg <myon@debian.org>
2022-06-30 14:58:27 +0200

    ci/rebuild.sh: Switch to date-based versions

--- ci/rebuild.sh
+++ ci/rebuild.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# This script prepares a rebuild of our debian package for testing
-# purpose.
-# It takes the last git tag and the number of commits since then.
-# Based on this information it generates a new changelog entry.
-#
-# E.g.: If we have 2 commits since tag 1.1 the new changelog will
-# represent version 1.1~2 which is lower than the actual version 1.1.
+# This script prepares a rebuild of our debian package for testing purposes.
 
 set -eu
 
@@ -20,8 +14,8 @@ orig_version=$(dpkg-parsechangelog -S version)
 if [ "$IS_RELEASE_BUILD" ]; then
   new_version="${orig_version}~$DISTTAG+1"
 else
-  commit_count=$(git log `git describe --tags --abbrev=0`..HEAD --oneline | wc -l)
-  new_version="${orig_version}~$DISTTAG~${commit_count}"
+  date="$(date -u +%Y%m%d.%H%M%S)"
+  new_version="${orig_version}~$DISTTAG~${date}"
 fi
 
 dch --force-bad-version -v ${new_version} "Automatic CI rebuild"
